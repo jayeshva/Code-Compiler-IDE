f7d373924e2ab2902cefe00a35ac968d25e63bb6
JAYESH V A <83073900+jayeshva@users.noreply.github.com>
2023-04-25 10:37:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,58 +17,7 @@ app.post("/compile", function (req, res) {
     var input = req.body.input
     var lang = req.body.lang
     try {
-
-        if (lang == "Cpp") {
-            if (!input) {
-                var envData = { OS: "linux", cmd: "gcc" }; // (uses g++ command to compile )
-                compiler.compileCPP(envData, code, function (data) {
-                    if (data.output) {
-                        res.send(data);
-                    }
-                    else {
-                        res.send({ output: "error" })
-                    }
-                });
-            }
-            else {
-                var envData = { OS: "linux", cmd: "gcc" }; // (uses g++ command to compile )
-                compiler.compileCPPWithInput(envData, code, input, function (data) {
-                    if (data.output) {
-                        res.send(data);
-                    }
-                    else {
-                        res.send({ output: "error" })
-                    }
-                });
-            }
-        }
-        else if (lang == "Java") {
-            if (!input) {
-                var envData = { OS: "linux" };
-                compiler.compileJava(envData, code, function (data) {
-                    if (data.output) {
-                        res.send(data);
-                    }
-                    else {
-                        res.send({ output: "error" })
-                    }
-                })
-            }
-            else {
-                //if windows  
-                var envData = { OS: "linux" };
-                //else
-                compiler.compileJavaWithInput(envData, code, input, function (data) {
-                    if (data.output) {
-                        res.send(data);
-                    }
-                    else {
-                        res.send({ output: "error" })
-                    }
-                })
-            }
-        }
-        else if (lang == "Python") {
+        if (lang == "Python") {
             if (!input) {
                 var envData = { OS: "windows" };
                 compiler.compilePython(envData, code, function (data) {
@@ -87,7 +36,7 @@ app.post("/compile", function (req, res) {
                         res.send(data);
                     }
                     else {
-                        res.send({ output: "error" })
+                        res.send(data)
                     }
                 });
             }
